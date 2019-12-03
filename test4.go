eb9029fdc5917ad21f5a467deb8ae4960915ca35
邱国宾 <qiuguobin@gracepsy.com>
2019-12-03 16:03:03 +0800

    commit

---
+++ test4.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "lucy654123xlhb",
+		DB:       0,
+	})
+	client.Set("count", 1, 0)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go reduce4(client, &wg)
+	}
+	wg.Wait()
+
+	fmt.Println("done")
+}
+
+func reduce4(client *redis.Client, wg *sync.WaitGroup) {
+	ok, _ := lock(client)
+	if ok {
+		count, _ := client.Get("count").Int()
+		if count > 0 {
+			client.Decr("count")
+		}
+		unlock(client)
+	}
+	wg.Done()
+}
+
+func lock(client *redis.Client) (bool, error) {
+	return client.SetNX("suo", true, 0).Result()
+}
+
+func unlock(client *redis.Client) {
+	client.Del("suo")
+}
